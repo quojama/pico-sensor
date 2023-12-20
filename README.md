5721913249bab256bd04e83940b65a6c2bfa8cc2
quojama <giantkoyama@gmail.com>
2023-12-21 01:32:11 +0900

    image switcher

--- README.md
+++ README.md
@@ -1,23 +1,37 @@
-Tempurture, humidity, and pressure sensor by MicroPython Rapsberry Pi Pico W (or Pico).
+Bosch社製のセンサーBME280を使って温湿度と大気圧をOLEDに表示するサンプルです。タクトスイッチを押すことで、画像を切り替えるオマケ機能付きです。
 
 ![image](https://github.com/quojama/pico-sensor/assets/4445606/f22d2cfd-139e-4baa-9424-44ad3c23e016)
 
-## Components
+## 使うパーツ (秋月電子で調達)
 
 - [Raspberry Pi Pico W](https://akizukidenshi.com/catalog/g/gM-17947/) (or [Pico](https://akizukidenshi.com/catalog/g/gM-16132/))
 - [BME280 sensor module](https://akizukidenshi.com/catalog/g/gK-09421/)
 - [0.96" SSD1306 OLED display](https://akizukidenshi.com/catalog/g/gP-12031/)
+- [Tactile switch](https://akizukidenshi.com/catalog/g/gP-08073/)
+- [3mm blue LED](https://akizukidenshi.com/catalog/g/gI-13233/)
+  - LEDはなんでもいいですし、なくてもいいです。ただし繋がない場合は該当コードを削除してください。
+- [Breadboard](https://akizukidenshi.com/catalog/g/gP-05294/)
+  - こちらも必要に応じて出問題なしです。
 
+## つなぎ方
 
-## Wiring
+| BME280 | Raspberry Pi Pico (W) | SSD1306 | LED     | Tactile switch |
+| ------ | --------------------- | ------- | ------- | -------------- |
+| VCC    | 3V3 (36pin)           | VCC     | -       | -              |
+| GND    | GND                   | GND     | Cathode | GND            |
+| -      | GP7 (10pin)           | SCL     | -       | -              |
+| -      | GP6 (9pin)            | SDA     | -       | -              |
+| CSB    | -                     | -       | -       | -              |
+| SDI    | GP4 (6pin)            | -       | -       | -              |
+| SDO    | GND                   | -       |         |                |
+| SCK    | GP5 (7pin)            | -       | -       | -              |
+| -      | GP10 (14pin)          | -       | Anode   | -              |
+| -      | GP19 (25pin)          | -       | -       | To GND         |
 
-| BME280 | Raspberry Pi Pico (W) | SSD1306 |
-| --- | --- | --- |
-| VCC | 3V3 (36pin) | VCC |
-| GND | GND | GND |
-| - | GP7 (10pin) | SCL |
-| - | GP6 (9pin) | SDA |
-| CSB | - | - |
-| SDI | GP4 (6pin) | - |
-| SDO | GND | - |
-| SCK | GP5 (7pin) | - |
+## 画像の作り方
+
+ディスプレイのサイズは128x64ドットです。何らかの方法で128x64pxのモノクロ二階調の画像を作成してください。JPGでもPNGでも問題ないです。
+
+次に [img2cc](https://javl.github.io/image2cpp/)p を使って画像をプレーンのビットマップに変換します。最後のOutputの項目にあるCode output formatはPlain bytesを選択してください。
+
+そちらのビットマップの文字列を.txtとして /img フォルダの中に保存するだけで追加されます。
--- main.py
+++ main.py
@@ -1,73 +1,102 @@
+import os
+
 import bme280
+import framebuf
 import ssd1306
 import utime
 from machine import I2C, Pin
-import framebuf
 
-# image slect
-image_dir = "/img/yacht.txt"
+# タクトスイッチのためにGPIO 19、内蔵プルアップ抵抗を使います。
+button = Pin(19, Pin.IN, Pin.PULL_UP)
 
-# pico w on board LED init
+# `/img` フォルダ内のテキストファイル名のリスト
+image_files = os.listdir("/img")
+image_files = [file for file in image_files if file.endswith(".txt")]
+current_image_index = 0
+print(image_files)
+
+# 2つのLEDの設定。内蔵のLEDをPico Wで使う場合は "LED" とし、
+# 無印Picoの場合は25とします
+# 今回は内蔵LEDを画像切替の時のみ点灯、外部LEDを常時点灯とします
 led = Pin("LED", Pin.OUT)
+# 外部LEDの設定と常時点灯。適当な抵抗も忘れないように。
+ext_led = Pin(10, Pin.OUT)
+ext_led.value(1)
 
-# SSD1306 init (SCL: GP6, SDA: GP7)
+# SSD1306の初期化 (SCL: GP6, SDA: GP7)
 i2c_ssd = I2C(1, scl=Pin(7), sda=Pin(6), freq=400000)
 oled = ssd1306.SSD1306_I2C(128, 64, i2c_ssd)
 
-# BME280 init (SCL: GP4, SDA: GP5)
+# BME280センサーの初期化 (SCL: GP4, SDA: GP5)
 i2c_bme = I2C(0, scl=Pin(5), sda=Pin(4))
 bme = bme280.BME280(i2c=i2c_bme)
 
-# image show func
+# 画像表示
 def display_image(x, y, width, height, data):
     image = framebuf.FrameBuffer(data, width, height, framebuf.MONO_HLSB)
     oled.blit(image, x, y)
 
-# loading plain bytes from image dir
+# 画像データをロード
 def load_image_data(filename):
     with open(filename, 'r') as file:
         data = file.read()
         byte_values = [int(b.strip(), 16) for b in data.split(',')]
         return bytearray(byte_values)
-image_data = load_image_data(image_dir)
 
+# 次の画像をロード
+def load_next_image():
+    global current_image_index
+    if image_files:
+        # 次の画像にインデックスを更新
+        current_image_index = (current_image_index + 1) % len(image_files)
+        image_path = '/img/' + image_files[current_image_index]
+        return load_image_data(image_path)
+    return None
+
+# 起動時の画像をロード
+if image_files:
+    image_data = load_image_data('/img/' + image_files[current_image_index])
+
+# ここからメインのループ
 while True:
-    
-    # get data from BME280
+    # タクトスイッチが押されたら外付けLEDを点灯し、
+    # 次の画像を読み込む関数を実行
+    # 1.5秒スリープして外付けLEDを消灯
+    if not button.value():
+        led.value(1)
+        image_data = load_next_image()
+        utime.sleep(1.5)
+        led.value(0)
+
+    # BME280からのデータ
     temperature, pressure, humidity = bme.read_compensated_data()
 
-    # format data
+    # データをフォーマット
     temperature = int(temperature / 100)
     humidity = int(humidity / 1024)
     pressure = int(pressure / 256 / 100)
 
-    temp_text = "Temp:{}C".format(temperature)
+    # データ表示用のテキスト
+    temp_text = "Temp :{}C".format(temperature)
     humidity_text = "Humid:{}%".format(humidity)
-    pressure_text = "Pres:{:,}hPa".format(pressure)
+    pressure_text = "Press:{:,}hPa".format(pressure)
 
-    # bink built in LED
-    led.value(1)
-    utime.sleep(0.2)
-    led.value(0)
-
-    # clean-up OLED display
+    # OLEDをリセット
     oled.fill(0)
 
-    # render loading image
+    # 画像を表示
     display_image(0, 0, 128, 64, image_data)
 
-    # text
+    # テキスト部分
     oled.text(temp_text, 5, 5)
     oled.text(humidity_text, 5, 18)
     oled.text(pressure_text, 5, 30)
-    oled.text("Good Luck!", 5, 52)
+    oled.text("Merry Xmas", 3, 51)
 
-    #box
-    oled.hline(0, 1, 120, 1) #top
-    oled.hline(0, 41, 120, 1) #bottom
+    # テキストを囲む四角
+    oled.hline(0, 1, 125, 1) #top
+    oled.hline(0, 41, 125, 1) #bottom
     oled.vline(0, 1, 41, 1) #left
-    oled.vline(120, 1, 41, 1) #right
+    oled.vline(125, 1, 41, 1) #right
 
     oled.show()
-
-    utime.sleep(10)
